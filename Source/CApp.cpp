0803a8aa17b5544c4b9cd97b2edd194fcfca8e1a
Zer0Dash <rage09274@gmail.com>
2026-03-02 15:03:41 +0300

    Update CApp.cpp

--- Source/CApp.cpp
+++ Source/CApp.cpp
@@ -1,1 +1,14 @@
+template<typename num> struct Point {
+    num x;
+    num y;
+};
 
+template<typename num> struct Segment {
+    Point<num> p1;
+    Point<num> p2;
+};
+
+template<typename num> struct Circle {
+    Point<num> center;
+    double R;
+};
