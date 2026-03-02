00b05436750553f94da6ba83864e9d707e572cc8
GrigoriyRemizov <rgrigoriy2412@gmail.com>
2026-03-02 16:15:48 +0300

    fix: cpp and h matrix

--- Source/Matrix.h
+++ Source/Matrix.h
@@ -1,13 +1,23 @@
 #include <stddef.h>
 
+struct IndexError
+{
+    char *log;
+};
+
 class Matrix {
     bool **array_;
     size_t cols_;
     size_t rows_;
+    static const IndexError indexError_;
+    void checkIndexes_(size_t row, size_t col) const;
+
 public:
     Matrix(size_t height, size_t width);
 
     ~Matrix();
 
-    bool get(size_t row, size_t col);
+    bool get(size_t row, size_t col) const;
+
+    void set(size_t row, size_t col, bool state);
 };
